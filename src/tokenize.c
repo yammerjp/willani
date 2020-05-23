4b0719541d11c761c4a38ff6ce04dbd5d28f7557
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-05-23 19:28:47 +0900

    Refactoring. Add identifer_token_length()

--- src/tokenize.c
+++ src/tokenize.c
@@ -70,6 +70,17 @@ static int reserved_token_length(char *p) {
   return 0;
 }
 
+static int identifer_token_length(char *p) {
+  if (!isalpha(*p)) {
+    return 0;
+  }
+  int i = 1;
+  while(isalnum(*(p+i))) {
+    i++;
+  }
+  return i;
+}
+
 Token *tokenize(char *p) {
   Token head = {};
   Token *current = &head;
@@ -93,17 +104,18 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    if ('a' <= *p && *p <= 'z' ) {
-      current = new_token(TK_IDENT, current,p++, 1, 0);
+    int ilen = identifer_token_length(p);
+    if ( ilen == 1 ) {
+      current = new_token(TK_IDENT, current, p++, ilen, 0);
       continue;
     }
 
-    int length = reserved_token_length(p);
-    if (length == 0) {
+    int rlen = reserved_token_length(p);
+    if (rlen == 0) {
       error_at(current, "Invalid token");
     }
-    current = new_token(TK_RESERVED, current, p ,length, 0); //with update p
-    p += length;
+    current = new_token(TK_RESERVED, current, p ,rlen, 0); //with update p
+    p += rlen;
     continue;
   }
 
