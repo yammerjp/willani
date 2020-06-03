4a7a7d11e8be67590acd6c0317e3fc02cb8ac7f6
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-06-03 15:52:27 +0900

    Delete garbage

--- src/main.c
+++ src/main.c
@@ -2,16 +2,10 @@
 
 char *user_input;
 
-static void init(void) {
-  gvars = NULL;
-}
-
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("%s: invalid number of arguments.\n", argv[0]);
   }
-
-  init();
 
   user_input = argv[1];
   Token *token = tokenize(user_input);
