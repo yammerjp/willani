6eb96369a53f6ac96395015f8c2f41337b3ae70e
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-06-06 23:43:30 +0900

    Fix to read escape charactors

--- src/tokenize.c
+++ src/tokenize.c
@@ -121,7 +121,7 @@ static int string_token_length(char *p) {
 
   for (int i = 1; p[i] && p[i] != '\n'; i++) {
     if (p[i] == '\\')
-      i++;
+      i+=2;
     if (p[i] == '"')
       return i+1;
   }
