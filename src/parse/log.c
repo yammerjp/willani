a6a0dc9dc4debc270d30f76d71b64c4d2ef16781
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-06-27 11:26:27 +0900

    Fix format

--- src/parse/log.c
+++ src/parse/log.c
@@ -34,55 +34,59 @@ static void print_type(FILE *file, Type *type) {
 
 void print_node(FILE *file, Node *node) {
   switch (node->kind) {
-    case ND_EXPR_ADD:            fprintf(file, "+"); break;
-    case ND_EXPR_PTR_ADD:        fprintf(file, "+ (pointer)"); break;
-    case ND_EXPR_SUB:            fprintf(file, "-"); break;
-    case ND_EXPR_PTR_SUB:        fprintf(file, "- (pointer)"); break;
-    case ND_EXPR_PTR_DIFF:       fprintf(file, "- (pointers diff)"); break;
-    case ND_EXPR_MUL:            fprintf(file, "*"); break;
-    case ND_EXPR_DIV:            fprintf(file, "/"); break;
-    case ND_EXPR_MOD:            fprintf(file, "%%"); break;
-    case ND_EXPR_EQ:             fprintf(file, "=="); break;
-    case ND_EXPR_NE:             fprintf(file, "!="); break;
-    case ND_EXPR_LESS_THAN:      fprintf(file, "<"); break;
-    case ND_EXPR_LESS_EQ:        fprintf(file, "<="); break;
-    case ND_EXPR_ASSIGN:         fprintf(file, "="); break;
-    case ND_EXPR_ASSIGN_ADD:     fprintf(file, "+="); break;
-    case ND_EXPR_ASSIGN_PTR_ADD: fprintf(file, "(pointer) +="); break;
-    case ND_EXPR_ASSIGN_SUB:     fprintf(file, "-="); break;
-    case ND_EXPR_ASSIGN_PTR_SUB: fprintf(file, "(pointer) -="); break;
-    case ND_EXPR_ASSIGN_MUL:     fprintf(file, "*="); break;
-    case ND_EXPR_ASSIGN_DIV:     fprintf(file, "/="); break;
-    case ND_EXPR_ASSIGN_MOD:     fprintf(file, "%%="); break;
-    case ND_EXPR_VAR:            fprintf(file, "%.*s",node->var->namelen, node->var->name); break;
-    case ND_EXPR_NUM:            fprintf(file, "%ld",node->value); break;
-    case ND_EXPR_STRING:         fprintf(file, "%.*s",node->token->length, node->token->location); break;
-    case ND_EXPR_ADDR:           fprintf(file, "&"); break;
-    case ND_EXPR_DEREF:          fprintf(file, "*"); break;
-    case ND_EXPR_MEMBER:         fprintf(file, ".%.*s", node->member->namelen, node->member->name); break;
-    case ND_STMT_RETURN:         fprintf(file, "return"); break;
-    case ND_STMT_IF:             fprintf(file, "if"); break;
-    case ND_STMT_WHILE:          fprintf(file, "while"); break;
-    case ND_STMT_FOR:            fprintf(file, "for"); break;
-    case ND_STMT_BLOCK:          fprintf(file, "{}"); break;
-    case ND_EXPR_FUNC_CALL:      fprintf(file, "%.*s()",node->func_namelen, node->func_name); break;
-    case ND_STMT_WITH_EXPR:      fprintf(file, ";"); break;
-    case ND_STMT_CONTINUE:       fprintf(file, "continue"); break;
-    case ND_STMT_BREAK:          fprintf(file, "break"); break;
-    case ND_STMT_SWITCH:         fprintf(file, "switch"); break;
-    case ND_LABEL_DEFAULT:       fprintf(file, "case %dth", node->case_num); break;
-    case ND_LABEL_CASE:          fprintf(file, "default"); break;
-    case ND_EXPR_WITH_STMTS:     fprintf(file, "({})"); break;
-    case ND_EXPR_TERNARY:        fprintf(file, "?:"); break;
-    case ND_EXPR_COMMA:          fprintf(file, ","); break;
-    case ND_EXPR_NOT:            fprintf(file, "!"); break;
-    case ND_EXPR_BIT_NOT:        fprintf(file, "~"); break;
-    case ND_EXPR_PRE_INC:        fprintf(file, "++ ..."); break;
-    case ND_EXPR_PRE_DEC:        fprintf(file, "-- ..."); break;
-    case ND_EXPR_POST_INC:       fprintf(file, "... ++"); break;
-    case ND_EXPR_POST_DEC:       fprintf(file, "... --"); break;
-    case ND_EXPR_LOG_OR:         fprintf(file, "||"); break;
-    case ND_EXPR_LOG_AND:        fprintf(file, "&&"); break;
+    // single operator
+    case ND_EXPR_VAR:             fprintf(file, "%.*s",node->var->namelen, node->var->name); break;
+    case ND_EXPR_NUM:             fprintf(file, "%ld",node->value); break;
+    case ND_EXPR_STRING:          fprintf(file, "%.*s",node->token->length, node->token->location); break;
+    case ND_EXPR_FUNC_CALL:       fprintf(file, "%.*s()",node->func_namelen, node->func_name); break;
+    case ND_EXPR_ADDR:            fprintf(file, "&"); break;
+    case ND_EXPR_DEREF:           fprintf(file, "*"); break;
+    case ND_EXPR_NOT:             fprintf(file, "!"); break;
+    case ND_EXPR_BIT_NOT:         fprintf(file, "~"); break;
+    case ND_EXPR_PRE_INC:         fprintf(file, "++ ..."); break;
+    case ND_EXPR_PRE_DEC:         fprintf(file, "-- ..."); break;
+    case ND_EXPR_POST_INC:        fprintf(file, "... ++"); break;
+    case ND_EXPR_POST_DEC:        fprintf(file, "... --"); break;
+    // dual operator
+    case ND_EXPR_ASSIGN:          fprintf(file, "="); break;
+    case ND_EXPR_ASSIGN_ADD:      fprintf(file, "+="); break;
+    case ND_EXPR_ASSIGN_PTR_ADD:  fprintf(file, "(pointer) +="); break;
+    case ND_EXPR_ASSIGN_SUB:      fprintf(file, "-="); break;
+    case ND_EXPR_ASSIGN_PTR_SUB:  fprintf(file, "(pointer) -="); break;
+    case ND_EXPR_ASSIGN_MUL:      fprintf(file, "*="); break;
+    case ND_EXPR_ASSIGN_DIV:      fprintf(file, "/="); break;
+    case ND_EXPR_ASSIGN_MOD:      fprintf(file, "%%="); break;
+    case ND_EXPR_ADD:             fprintf(file, "+"); break;
+    case ND_EXPR_PTR_ADD:         fprintf(file, "+ (pointer)"); break;
+    case ND_EXPR_SUB:             fprintf(file, "-"); break;
+    case ND_EXPR_PTR_SUB:         fprintf(file, "- (pointer)"); break;
+    case ND_EXPR_PTR_DIFF:        fprintf(file, "- (pointers diff)"); break;
+    case ND_EXPR_MUL:             fprintf(file, "*"); break;
+    case ND_EXPR_DIV:             fprintf(file, "/"); break;
+    case ND_EXPR_MOD:             fprintf(file, "%%"); break;
+    case ND_EXPR_EQ:              fprintf(file, "=="); break;
+    case ND_EXPR_NE:              fprintf(file, "!="); break;
+    case ND_EXPR_LESS_THAN:       fprintf(file, "<"); break;
+    case ND_EXPR_LESS_EQ:         fprintf(file, "<="); break;
+    case ND_EXPR_MEMBER:          fprintf(file, ".%.*s", node->member->namelen, node->member->name); break;
+    case ND_EXPR_COMMA:           fprintf(file, ","); break;
+    case ND_EXPR_LOG_OR:          fprintf(file, "||"); break;
+    case ND_EXPR_LOG_AND:         fprintf(file, "&&"); break;
+    // multiple operator
+    case ND_EXPR_TERNARY:         fprintf(file, "?:"); break;
+    case ND_EXPR_WITH_STMTS:      fprintf(file, "({})"); break;
+    // statement
+    case ND_STMT_BLOCK:           fprintf(file, "{}"); break;
+    case ND_STMT_IF:              fprintf(file, "if"); break;
+    case ND_STMT_SWITCH:          fprintf(file, "switch"); break;
+    case ND_STMT_WHILE:           fprintf(file, "while"); break;
+    case ND_STMT_FOR:             fprintf(file, "for"); break;
+    case ND_STMT_RETURN:          fprintf(file, "return"); break;
+    case ND_STMT_WITH_EXPR:       fprintf(file, ";"); break;
+    case ND_STMT_CONTINUE:        fprintf(file, "continue"); break;
+    case ND_STMT_BREAK:           fprintf(file, "break"); break;
+    case ND_LABEL_CASE:           fprintf(file, "default"); break;
+    case ND_LABEL_DEFAULT:        fprintf(file, "case %dth", node->case_num); break;
     default : error_at(node->token->location, "unexpected node->kind");
   }
 
