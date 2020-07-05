99e7d7cf4e557d2550983a4217c7c69a0ef15d94
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-07-05 17:19:05 +0900

    Refactoring to divide function

--- src/parse/var_init.c
+++ src/parse/var_init.c
@@ -1,38 +1,39 @@
 #include "parse.h"
 
+static Node *read_array(Token **rest, Token *token, Type *type, long *values, int offset, Var *var);
+static Node *read_cell(Token **rest, Token *token, long *values, int offset, Var *var);
+static void *read_array_string(Token **rest, Token *token, Type *type, long *values, int offset, Var *var);
+
+// declare and initialize variable statement = type declarator type_suffix "=" read_var_init ";"
+// read_var_init                             = num | "{" read_var_init ("," read_var_init)* ","? "}"
+Node *read_var_init(Token **rest, Token *token, Var *var) {
+  // mesure size
+  int size = 1;
+  for (Type *ty = var->type; ty->kind == TYPE_ARRAY; ty = ty->base)
+    size *= ty->array_length;
+
+  long *values = calloc(size, sizeof(long));
+
+  Node *runtime_inits = read_array(rest, token, var->type, values, 0, var);
+  if (var->is_global && runtime_inits)
+    error_at(token, "failed that global variable is initilized by run-time-settled value");
+
+  Node *node = new_node_var_init(token, var, values, size);
+  node->next = runtime_inits;
+  return node;
+}
+
 static Node *read_array(Token **rest, Token *token, Type *type, long *values, int offset, Var *var) {
   Node head = {};
   Node *tail = &head;
-  if (type->kind != TYPE_ARRAY) {
-    // array cell init
-    if (is_number_token(token)) {
-      // array cell init by number
-      values[offset] = str_to_l(token->location, token->length);
-      *rest = token->next;
-      return NULL;
-    }
-    if (var->is_global)
-      error_at(token, "expected number token to initialize variable");
 
-    // array cell init by assign
-    Node *left = new_node_var_specified(var, token);
-    while (left->type->kind == TYPE_ARRAY)
-      left = new_node_deref(left, token);
-
-    Node *right = assign(&token, token);
-    *rest = token;
-    return new_node_expr_stmt(new_node_assign(left, right, token), token);
-  }
+  // array cell init
+  if (type->kind != TYPE_ARRAY)
+    return read_cell(rest, token, values, offset, var);
 
   // array row init by string
   if (type->base->kind != TYPE_ARRAY && is_string_token(token)) {
-    String *string = new_string(token->location+1, token->length-2);
-    if (type->array_length < string->length)
-      error_at(token, "string token is too longer than array length");
-
-    for (int i=0; i < string->length; i++)
-      values[offset+i] = (string->p)[i];
-    *rest = token->next;
+    read_array_string(rest, token, type, values, offset, var);
     return NULL;
   }
 
@@ -45,7 +46,7 @@ static Node *read_array(Token **rest, Token *token, Type *type, long *values, in
   for (Type *ty = type->base; ty->kind == TYPE_ARRAY; ty = ty->base)
     cell_size *= ty->array_length;
 
-  for (int i=0; i<type->array_length; i++) {
+  for (int i=0; i < type->array_length; i++) {
     if (equal(token, "}"))
       break;
     tail->next = read_array(&token, token, type->base, values, offset + (cell_size * i), var);
@@ -66,21 +67,32 @@ static Node *read_array(Token **rest, Token *token, Type *type, long *values, in
   return head.next;
 }
 
-// declare and initialize variable statement = type declarator type_suffix "=" read_var_init ";"
-// read_var_init                             = num | "{" read_var_init ("," read_var_init)* ","? "}"
-Node *read_var_init(Token **rest, Token *token, Var *var) {
-  // mesure size
-  int size = 1;
-  for (Type *ty = var->type; ty->kind == TYPE_ARRAY; ty = ty->base)
-    size *= ty->array_length;
+static Node *read_cell(Token **rest, Token *token, long *values, int offset, Var *var) {
+  if (is_number_token(token)) {
+    // array cell init by number
+    values[offset] = str_to_l(token->location, token->length);
+    *rest = token->next;
+    return NULL;
+  }
+  if (var->is_global)
+    error_at(token, "expected number token to initialize variable");
 
-  long *values = calloc(size, sizeof(long));
+  // array cell init by assign
+  Node *left = new_node_var_specified(var, token);
+  while (left->type->kind == TYPE_ARRAY)
+    left = new_node_deref(left, token);
 
-  Node *runtime_inits = read_array(rest, token, var->type, values, 0, var);
-  if (var->is_global && runtime_inits)
-    error_at(token, "failed that global variable is initilized by run-time-settled value");
+  Node *right = assign(&token, token);
+  *rest = token;
+  return new_node_expr_stmt(new_node_assign(left, right, token), token);
+}
 
-  Node *node = new_node_var_init(token, var, values, size);
-  node->next = runtime_inits;
-  return node;
+static void *read_array_string(Token **rest, Token *token, Type *type, long *values, int offset, Var *var) {
+  String *string = new_string(token->location+1, token->length-2);
+  if (type->array_length < string->length)
+    error_at(token, "string token is too longer than array length");
+
+  for (int i=0; i < string->length; i++)
+    values[offset+i] = (string->p)[i];
+  *rest = token->next;
 }
