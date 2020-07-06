9bdfff18519098e64eb20d0e5c5a5bd8e8c9b875
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-07-06 15:23:47 +0900

    Fix bug to tokenize reserved token

--- src/tokenize.c
+++ src/tokenize.c
@@ -120,7 +120,7 @@ static int identifer_token_length(char *p) {
 }
 
 static int reserved_token_length(char *p) {
-  char reserved_words[][8] = {
+  char reserved_words[][9] = {
     "continue",
     "default",
     "typedef",
