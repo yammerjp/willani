a77bf805680352ddbaec851fe7bd9a8ebed59c82
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-07-10 19:25:22 +0900

    Refactoring to add read_args()

--- src/main.c
+++ src/main.c
@@ -8,54 +8,54 @@ int is_printing_asm_debug = false;
 int is_printing_tokenize_log = false;
 int is_printing_node_log = false;
 int is_printing_parse_log = false;
+static char *input_filename = NULL;
+static char *output_filename = NULL;
 
 static void help(char * argv0);
 
-int main(int argc, char **argv) {
-  char *input_filename = NULL;
-  char *output_filename = NULL;
+bool find_strs(char *target, char* str1, char* str2) {
+  int len = strlen(target);
+  if (len == strlen(str1) && !strncmp(target, str1, len))
+    return true;
+  if (len == strlen(str2) && !strncmp(target, str2, len))
+    return true;
+  return false;
+}
 
+void read_args(int argc, char **argv) {
   for (int i=1; i < argc; i++) {
-    int len = strlen(argv[i]);
-    if (len==2 && !strncmp("-h", argv[i], 2)
-     || len==6 && !strncmp("--help", argv[i], 6)) {
+    if (find_strs(argv[i], "-h", "--help")) {
       help(argv[0]);
     }
-
-    if (len==2 && !strncmp("-o", argv[i], 2)
-     || len==8 && !strncmp("--output", argv[i], 8)) {
+    if (find_strs(argv[i], "-o", "--output")) {
       if (++i >= argc)
         error("Usage: %s inputfile -o outputfile", argv[0]);
       output_filename = argv[i];
       continue;
     }
-    if (len==2 && !strncmp("-a", argv[i], 2)
-     || len==5 && !strncmp("--ast", argv[i], 5)) {
+    if (find_strs(argv[i], "-a", "--ast")) {
       is_printing_ast = true;
       continue;
     }
-    if (len==2 && !strncmp("-d", argv[i], 2)
-     || len==7 && !strncmp("--debug", argv[i], 7)) {
+    if (find_strs(argv[i], "-d", "--debug")) {
       is_printing_asm_debug = true;
       continue;
     }
-    if (len==3 && !strncmp("-tl", argv[i], 3)
-     || len==14 && !strncmp("--tokenize-log", argv[i], 14)) {
+    if (find_strs(argv[i], "-tl", "--tokenize-log")) {
       is_printing_tokenize_log = true;
       continue;
     }
-    if (len==3 && !strncmp("-nl", argv[i], 3)
-     || len==10 && !strncmp("--node-log", argv[i], 10)) {
+    if (find_strs(argv[i], "-nl", "--node-log")) {
       is_printing_node_log = true;
       continue;
     }
-    if (len==3 && !strncmp("-pl", argv[i], 3)
-     || len==11 && !strncmp("--parse-log", argv[i], 11)) {
+    if (find_strs(argv[i], "-pl", "--parse-log")) {
       is_printing_parse_log = true;
       continue;
     }
     if (argv[i][0] == '-')
       error("unknown option: %s", argv[i]);
+
     if (input_filename != NULL)
       error("duplicated input files");
     input_filename = argv[i];
@@ -64,6 +64,11 @@ int main(int argc, char **argv) {
 
   if (!output_filename || !input_filename)
     error("Usage: %s inputfile -o outputfile", argv[0]);
+}
+
+int main(int argc, char **argv) {
+  // read commandline arguments and flag options
+  read_args(argc, argv);
 
   user_input = read_file(input_filename);
   Token *token = tokenize(user_input, input_filename);
@@ -73,8 +78,6 @@ int main(int argc, char **argv) {
   program(token);
 
   code_generate(output_filename);
-
-  return 0;
 }
 
 static void help(char *argv0) {
