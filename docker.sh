00363e0c882d73c4fdbf65929e6b225c17d1cdf9
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-07-25 16:38:50 +0900

    Add './docker.sh self'

--- docker.sh
+++ docker.sh
@@ -11,6 +11,8 @@ elif [ "$sub_command" = "sh"     ]; then
   docker run --rm -it -v "$REPO_DIR:/willani" -w /willani compilerbook
 elif [ "$sub_command" = "test"   ]; then
   docker run --rm -v "$REPO_DIR:/willani" -w /willani compilerbook make test
+elif [ "$sub_command" = "self"   ]; then
+  docker run --rm -v "$REPO_DIR:/willani" -w /willani compilerbook ./self.sh
 elif [ "$sub_command" = "sample" ]; then
   docker run --rm -v "$REPO_DIR:/willani" -w /willani compilerbook make sample
 elif [ "$sub_command" = "debug"  ]; then
@@ -19,6 +21,7 @@ else
   echo "usage: ./docker.sh pull   ... download and build Dockerfile"
   echo "usage: ./docker.sh sh     ... start interactive shell"
   echo "usage: ./docker.sh test   ... build and run test/test.c"
+  echo "usage: ./docker.sh self   ... check selfhosted compiler with running self.sh"
   echo "usage: ./docker.sh sample ... build and run sample.c"
   echo "usage: ./docker.sh debug  ... build and run sample.c on gdb"
 fi
